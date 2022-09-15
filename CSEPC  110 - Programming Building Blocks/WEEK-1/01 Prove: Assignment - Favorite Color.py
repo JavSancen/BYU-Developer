cb2a9fb9c482a02046c931996e5fd884fea93a28
JavSancen <fr.sancen@gmail.com>
2022-09-15 06:11:29 -0500

    adding changes in the name of the file assignment and adding notes to helloworld.py

--- CSEPC  110 - Programming Building Blocks/WEEK-1/01 Prove: Assignment - Favorite Color.py
+++ CSEPC  110 - Programming Building Blocks/WEEK-1/01 Prove: Assignment - Favorite Color.py
@@ -2,5 +2,5 @@
 
 name = input('What is your name?: ')
 color = input(name + ', What is your favorite color?: ')
-wow = input(color + '?, wow, and why?: ')
+wow = input(color + '?, and why?: ')
 print('That is very interesting!')
--- CSEPC  110 - Programming Building Blocks/WEEK-1/helloworld.py
+++ CSEPC  110 - Programming Building Blocks/WEEK-1/helloworld.py
@@ -1,13 +1,13 @@
 
-
+# Creating an input
 name = input('Enter you name:')
 print('hello ' + name)
 
-
-
+# inputs and outputs
 print('adding numbers')
 x = 50 + 100
 print(x)
+# Learning read an error
 print('dividing numbers')
 y = x / 0
 print(y)
