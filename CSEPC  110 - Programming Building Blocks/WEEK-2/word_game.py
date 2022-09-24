5e6d7a60c845be78a93ff5a05e7d46b87745072f
JavSancen <fr.sancen@gmail.com>
2022-09-24 13:03:29 -0500

    fixinng history game

--- CSEPC  110 - Programming Building Blocks/WEEK-2/word_game.py
+++ CSEPC  110 - Programming Building Blocks/WEEK-2/word_game.py
@@ -9,7 +9,7 @@ verb_two = input('Write a verb: ')
 verb_three = input('Write a verb: ')
 verb_four = input('Write a verb: ')
 exclamation = input('Write an exclamation: ')
-history = f'''A very long time ago in a faraway land where the birds {adjective} and the squirrels {verb} and cows were very {adjective_two}, there was a tree {verb_two} a lot, one day the tree {verb_four} so much that the {animal}s came from everywhere to live in him. The tree went so full that one day he said {exclamation} I need to do something, so he {verb_three} then he stay {adjective_three} so he decides to stay with the {animal}s to {verb_four}
+history = f'''In a faraway land where the birds were {adjective}, the squirrels {verb} and cows were very {adjective_two}, there was a tree enjoing to {verb_two} a lot, one day the tree {verb_four} so much that the {animal}s came from everywhere to live in him. The tree went so full that one day he said {exclamation} I need to do something, so he {verb_three} then he become {adjective_three} so he decides to stay with the {animal}s to {verb_four}
 '''
 print()
 
