30d5816c996e230dd12b864ee1bac110f16f8679
JavSancen <fr.sancen@gmail.com>
2022-09-30 07:44:43 -0500

    adding assignment of week 3

--- CSEPC  110 - Programming Building Blocks/WEEK-3/meal-calculator.py
+++ CSEPC  110 - Programming Building Blocks/WEEK-3/meal-calculator.py
@@ -3,8 +3,10 @@ child_meal_cost = float(input("What is the price of a child's meal? "))
 adult_meal_cost = float(input("What is the price of an adult's meal? "))
 
 # Asking for the number of children's and adult's
-children_number = float(input("How many children are there? "))
-adults_number = float(input("How many adults are there? "))
+children_number = int(input("How many children are there? "))
+adults_number = int(input("How many adults are there? "))
+
+tip = float(input("Do you want to add a tip? add the tip rate you wich: "))
 
 # Asking the sales tax rate
 tax = float(input("What is the sales tax rate? "))
@@ -12,14 +14,25 @@ print()
 
 # Computing and displaying breakdown of amounts
 subtotal = (child_meal_cost * children_number) + (adult_meal_cost * adults_number)
+tip_rate = subtotal * (tip / 100)
 sales_tax = subtotal * (tax / 100)
 total = subtotal + sales_tax
+including_tip = total + tip_rate
 print(f"Subtotal: ${subtotal}")
+print(f"Tip: ${tip_rate}")
 print(f"Sales Tax: ${sales_tax}")
 print(f"Total: ${total}")
+# Adding the tip rate
+print(f"Including Tip: ${including_tip}")
 print()
 
 # Computing payment amount and change
 payment_amount = float(input("What is the payment amount? "))
 change = payment_amount - total
-print(f"Change: {change}")
+change_including_tip = payment_amount - including_tip
+print(f"Change: ${change}")
+print()
+print("Do you want add the tip?")
+print("Yes.")
+print()
+print(f"Change: ${change_including_tip}")
