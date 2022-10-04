aa880e81419c6c42ecf0dbcc2005a8433d665c8d
JavSancen <fr.sancen@gmail.com>
2022-10-04 17:37:10 -0500

    adding assignment week 4

---
+++ CSEPC  110 - Programming Building Blocks/WEEK-4/grades-converter.py
@@ -0,0 +1,14 @@
+print()
+
+# Choicing a Fahrenheit temperature
+fahrenheit = float(input("What is the temperature in Fahrenheit? "))
+
+# Converting Fahrenheit to Celsius
+celsius = (fahrenheit - 32) * 5/9
+
+# Printing temperature in Celsius
+print(f"The temperature in Celsius is {celsius:.1f} degrees.")
+
+print()
+
+# I chose the temperature name because I wanted to make it obvious.
