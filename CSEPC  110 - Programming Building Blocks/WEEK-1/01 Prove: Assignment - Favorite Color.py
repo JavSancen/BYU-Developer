26ca01ba4a350e0d91d46a85a67f3aa595139a83
Francisco Sancen <fr.sancen@gmail.com>
2023-07-01 21:04:45 -0600

    adding deleted files

--- CSEPC  110 - Programming Building Blocks/WEEK-1/01 Prove: Assignment - Favorite Color.py
+++
@@ -1,6 +0,0 @@
-#01 Prove: Assignment - Favorite Color
-
-name = input('What is your name?: ')
-color = input(name + ', What is your favorite color?: ')
-wow = input(color + '?, and why?: ')
-print('That is very interesting!')
